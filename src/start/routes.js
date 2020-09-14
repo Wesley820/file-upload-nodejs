b3fca1bd9fea86009f8724cde7ab2afba5a58167
Wesley Oliveira <assiswesley549@gmail.com>
2020-09-14 10:54:54 -0300

    feat: added github actions with sonarcloud

--- src/start/routes.js
+++ src/start/routes.js
@@ -4,15 +4,15 @@ import multer from 'multer';
 import FileController from '@app/controllers/FileController';
 import multerConfig from '@config/multer';
 
-const Route = Router();
+const routes = Router();
 const upload = multer(multerConfig);
 
-Route.get('/', async (request, response) => {
+routes.get('/', async (request, response) => {
   return response.json({ greeting: 'Hello world' });
 });
 
-Route.post('/files', upload.single('file'), FileController.store);
-Route.get('/files', FileController.index);
-Route.delete('/files/:id', FileController.delete);
+routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/files', FileController.index);
+routes.delete('/files/:id', FileController.delete);
 
-export default Route;
+export default routes;
